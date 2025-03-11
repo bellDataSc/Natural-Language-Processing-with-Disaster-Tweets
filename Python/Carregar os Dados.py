003e956ae34c0efebc7ffff174273da2c3f4a546
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2025-03-11 17:37:59 -0300

    Create Carregar os Dados.py

---
+++ Python/Carregar os Dados.py
@@ -0,0 +1,6 @@
+# Carregar os dados de treino e teste
+train_data = pd.read_csv('/kaggle/input/nlp-getting-started/train.csv')
+test_data = pd.read_csv('/kaggle/input/nlp-getting-started/test.csv')
+
+# Visualizar as primeiras linhas dos dados de treino
+train_data.head()
